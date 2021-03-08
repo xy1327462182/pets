7e4bb31b585f40a948ebecfcf0236b15faa624f4
xy1327462182 <1327462182@qq.com>
2021-03-08 15:25:18 +0800

    完成首页布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,114 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    swiperList: [
+      {
+        id: 0,
+        url: '/static/swiper/swiper1.jpg',
+      },
+      {
+        id: 1,
+        url: '/static/swiper/swiper2.jpg',
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ],
+    baikeNews: [
+      {
+        id: 'sfzadsfsfd',
+        title: '金毛犬的性格你了解多少金毛犬的性格你了解多少金毛犬的性格你了解多少金',
+      },
+      {
+        id: 'sfzaasdsfsfd',
+        title: '怎么训练金毛捡东西',
+      },
+      {
+        id: 'rersfzaasdsfsfd',
+        title: '长毛吉娃娃如何照顾',
+      },
+    ],
+    trends: [
+      {
+        id: '343dssd',
+        images: ['/static/trends/trend_test.jpg','/static/trends/trend_test.jpg','/static/trends/trend_test.jpg'],
+        title: '两个月大的狗狗',
+        info: '经常喂养的狗狗，现在因为出差外出没法养，找一个好心人收养。经常喂养的狗狗，现在因为出差外出没法养，找一个好心人收养。经常喂养的狗狗，现在因为出差外出没法养，找一个好心人收养。',
+      },
+      {
+        id: '343d3434ssd',
+        images: ['/static/trends/trend_test.jpg','/static/trends/trend_test.jpg','/static/trends/trend_test.jpg'],
+        title: '一个月大的金毛',
+        info: '在因为出差外出没法养，找一个好心人收养。',
+      },
+      {
+        id: '343d3asd434ssd',
+        images: ['/static/trends/trend_test.jpg','/static/trends/trend_test.jpg','/static/trends/trend_test.jpg'],
+        title: '一个月大的金毛',
+        info: '在因为出差外出没法养，找一个好心人收养。',
+      },
+      {
+        id: '343d3aassd434ssd',
+        images: ['/static/trends/trend_test.jpg','/static/trends/trend_test.jpg','/static/trends/trend_test.jpg'],
+        title: '一个月大的金毛',
+        info: '在因为出差外出没法养，找一个好心人收养。',
+      },
+    ]
   },
-  getUserInfo(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
