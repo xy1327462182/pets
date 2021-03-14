77287cfda8f8a4ed29ff704ebc456a021c1dfac3
yang <1327462182@qq.com>
2021-03-14 16:46:44 +0800

    完成百科详情及领养信息详情

---
+++ pages/adopt_detail/adopt_detail.js
@@ -0,0 +1,83 @@
+// pages/adopt_detail/adopt_detail.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    data: {
+      title: '上海市的狗狗小黑黑涨啊在正在找家上海市的狗狗小黑黑涨啊在正在找家',
+      adopt: false,
+      hits: 23453,
+      date: '2021/02/09 8:00',
+      images: [
+        'http://pic.shuoshuokong.com/d/file/2020-05/1589872601385258.jpg',
+        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2396575955,1510657336&fm=26&gp=0.jpg',
+        'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1673234836,1998903344&fm=26&gp=0.jpg',
+      ],
+      content: '据记者了解，近日，新疆维吾尔自治区部分企业和民众正在委托律师起诉德国人郑国恩。当地民众称，郑国恩散布“强迫劳动”等涉疆谣言，致使他们的声誉受损并遭受经济损失。目前，他们已向新疆地方法院提起民事诉讼申请，要求郑国恩赔礼道歉、恢复名誉并赔偿损失。',
+      author: {
+        avatar: 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1736447247,230581483&fm=11&gp=0.jpg',
+        id: 'q23q',
+        username: '中华田园猫',
+        gender: 2, //0男 1女
+      }
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
